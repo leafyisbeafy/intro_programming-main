8bc96e09401b7272c589e37c7af8e3aa775e8819
Oshonik Kumar <workforoshonik@gmail.com>
2024-09-16 23:46:22 -0500

    main

---
+++ demos/week2/page29.py
@@ -0,0 +1,13 @@
+counter = 0
+score_total = 0
+test_score = 0
+
+while test_score != 999:
+    test_score = int(input("Enter test score: "))
+    if(test_score >= 0 and test_score <= 100):
+        score_total += test_score
+        counter += 1
+
+average_score = score_total / counter
+print("Total score: " + str(score_total))
+print("Average score: " + str(average_score))
